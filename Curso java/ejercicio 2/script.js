d1a31c4d664b1a1f11746a9ba5f18da74e0caf9c
geronaef03 <130709078+geronaef03@users.noreply.github.com>
2023-10-09 05:01:49 -0300

    Update script.js

--- Curso java/ejercicio 2/script.js
+++ Curso java/ejercicio 2/script.js
@@ -1,11 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
 
-    const promedioCells = document.querySelectorAll(".promedio-10");
-
-    promedioCells.forEach(function (cell) {
-        const promedio = parseFloat(cell.textContent);
-        if (promedio === 10) {
-            cell.classList.add("promedio-10");
-        }
-    });
-});
