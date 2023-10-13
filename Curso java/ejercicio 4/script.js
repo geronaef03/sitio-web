f50aed4dd20e47676e0b82c44efb21c03185cd3a
Geronimo Naef <geronaef03@gmail.com>
2023-10-13 17:18:00 -0300

    ejercicio 4

--- Curso java/ejercicio 4/script.js
+++ Curso java/ejercicio 4/script.js
@@ -1,59 +1,83 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const openModalButton = document.getElementById("openModalButton");
-    const closeModalButton = document.getElementById("closeModalButton");
-    const modal = document.getElementById("myModal");
-    const numberInput = document.getElementById("numberInput");
-    const resultElement = document.getElementById("result");
+const modal = document.getElementById("miModal");
+const botonCerrar = document.querySelector(".cerrar-modal");
 
-    openModalButton.addEventListener("click", function () {
-        modal.style.display = "block";
-    });
+const productos = []; productos.forEach((producto, index) => {
+  const productoElement = document.querySelectorAll(".producto")[index];
+  productoElement.addEventListener("click", () => {
+    const imagenProducto = document.getElementById("imagen-producto");
+    const descripcionProducto = document.getElementById("descripcion-producto");
+    imagenProducto.src = producto.image_field;
+    descripcionProducto.textContent = producto.description;
 
-    closeModalButton.addEventListener("click", function () {
-        modal.style.display = "none";
-    });
-
-    const confirmButton = document.getElementById("confirmButton");
-    confirmButton.addEventListener("click", function () {
-        const number = parseFloat(numberInput.value);
-
-        if (!isNaN(number)) {
-            resultElement.textContent = number;
-            modal.style.display = "none";
-        } else {
-            alert("Por favor, ingresa un número válido.");
-        }
-    });
+    modal.style.display = "block";
+  });
 });
 
-const url = "https://portal.edu-apps.com.ar/api/products/?format=json";
-const clase = JSON.parse(claseJSON)
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    
-    mostrarProductos(data);
-  })
-  .catch((error) => {
-    console.error("Error al cargar los datos:", error);
-  });
+botonCerrar.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
-  function mostrarProductos(productos) {
-    const contenedor = document.getElementById("productos-container");
-  
-    productos.forEach((producto) => {
-      const productoElement = document.createElement("div");
-      productoElement.classList.add("producto");
-        
-      // Agrega el nombre y el precio del producto
-      productoElement.innerHTML = `
-        <h3>${Onix}</h3>
-        <p>Precio: $${13000000.0}</p>
-        <h3>${Yaris}</h3>
-        <p>Precio: $${14000000.0}</p>
-      `;
-  
-      // Inserta el producto en el contenedor
-      contenedor.appendChild("productos-container");
+
+const url = "https://portal.edu-apps.com.ar/api/products/?format=json";
+
+function cargarProductos() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      mostrarProductos(data);
+    })
+  .catch((error) => {
+      console.error("Error al cargar los datos:", error);
     });
-  }
+    
+}
+
+function mostrarProductos(productos) {
+  const contenedor = document.getElementById("productos-container");
+  contenedor.innerHTML = '';
+
+  // Encuentra el producto más caro
+  const productoMasCaro = productos.reduce((max, producto) =>
+    producto.price > max.price ? producto : max
+  );
+
+  productos.forEach((producto) => {
+    const productoElement = document.createElement("div");
+    productoElement.classList.add("producto");
+
+    // Aplica un borde de color a los productos más caros
+    if (producto.price === productoMasCaro.price) {
+      productoElement.classList.add("producto-destacado");
+    }
+
+    productoElement.innerHTML = `
+      <h3>${producto.name}</h3>
+      <p>Precio: ${producto.price.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</p>
+    `;
+
+    productoElement.addEventListener("click", () => {
+      const modal = document.getElementById("miModal");
+      const imagenProducto = document.getElementById("imagen-producto");
+      const descripcionProducto = document.getElementById("descripcion-producto");
+
+      imagenProducto.src = producto.image_field;
+      descripcionProducto.textContent = producto.description;
+
+      modal.style.display = "block";
+    });
+
+    contenedor.appendChild(productoElement);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  cargarProductos();
+});
+
+cargarProductos();
+
+function cargarDatos() {
+  cargarDatos();
+}
+
+setInterval(cargarDatos, 300000);
